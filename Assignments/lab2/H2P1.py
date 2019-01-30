42b73277390ba2cda41bc7728ee7740960f3e918
KelseySlavin <klslavin412@gmail.com>
2019-01-30 15:02:21 -0800

    Finishes Lab 2 Homework

---
+++ Assignments/lab2/H2P1.py
@@ -0,0 +1,6 @@
+age = int(input("What is your age?: "))
+older_age = age + 10
+print ("In 10 years you will be " +str(older_age)+ ".")
+degrees_fahrenheit = float(input("What is the current degrees in Fahrenheit?: "))
+degrees_celsius = float(degrees_fahrenheit*5/9)-32
+print ("It is "+str(degrees_celsius) +"in degrees celcius. ")
